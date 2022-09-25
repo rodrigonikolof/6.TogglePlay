7c3eecd79636aa30b7dcd549d1321fc28aba7449
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2022-09-25 20:53:02 +1000

    Added toggle to images at the top, div container stays in place and holds size

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
-const toggleImage = document.querySelectorAll('.togglePic');
+const toggleImageDiv = document.querySelectorAll('.togglePic');
+const toggleImageItself = document.querySelectorAll('.imgFile');
 const hideImage = document.querySelectorAll('.hideImage');
 
 
-for (let i = 0; i < toggleImage.length; i++){
-    toggleImage[i].addEventListener('click', function(){
-        this.classList.toggle('hideStuff');
-        if (this.style.visibility == 'visible'){
-            this.style.visibility == 'hidden';
-        } 
-        else {
-            this.style.visibility == 'visible';
-        }
+// for (let i = 0; i < toggleImageItself.length; i++){
+//     toggleImageItself[i].addEventListener('click', function(){
+//         this.classList.toggle('hideStuff');
 
+//         // if (this.style.visibility == 'visible'){
+//         //     this.style.visibility == 'hidden';
+//         // } 
+//         // else {
+//         //     this.style.visibility == 'visible';
+//         // }
+//     });
+// }
 
-    });
+for (let i= 0; i < toggleImageDiv.length; i++){
+    toggleImageDiv[i].addEventListener('click', function (){
+        toggleImageItself[i].classList.toggle('hideStuff');
+    })
 }
+
+
+
 
 const blepButton = document.querySelector('#blepButton');
 const blep = document.querySelector('#blep');
